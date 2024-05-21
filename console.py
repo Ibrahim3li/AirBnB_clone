4e2adefc301fe496617cf1d65eb96761a4482321
ibrahim3li <ibrahim3li.9619@gmail.com>
2024-05-21 22:05:47 +0300

    edit

--- console.py
+++ console.py
@@ -1,18 +1,14 @@
-#!/usr/bin/python3
-"""Module for the HBNB command interpreter."""
-
+#!usr/bin/python3
+"""module for HBNB commaan interprater"""
 import cmd
-from models import storage
 from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
+from models.engine.file_storage import FileStorage
+
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
+    file_storage = FileStorage()
+    file_storage.reload()
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
@@ -26,7 +22,7 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, arg):
-        """Creates a new instance of BaseModel or any other class, saves it (to the JSON file) and prints the id"""
+        """Creates a new instance of BaseModel and saves it"""
         if not arg:
             print("** class name missing **")
             return
@@ -38,81 +34,24 @@ class HBNBCommand(cmd.Cmd):
             print("** class doesn't exist **")
 
     def do_show(self, arg):
-        """Prints the string representation of an instance based on the class name and id"""
+        """Prints the string representation of an instance"""
         args = arg.split()
         if not args:
             print("** class name missing **")
             return
-        if args[0] not in storage.classes():
+        if args[0] not in HBNBCommand.file_storage.classes():
             print("** class doesn't exist **")
             return
         if len(args) < 2:
             print("** instance id missing **")
             return
         key = "{}.{}".format(args[0], args[1])
-        if key in storage.all():
-            print(storage.all()[key])
+        if key in HBNBCommand.file_storage.all():
+            print(HBNBCommand.file_storage.all()[key])
         else:
             print("** no instance found **")
 
-    def do_destroy(self, arg):
-        """Deletes an instance based on the class name and id"""
-        args = arg.split()
-        if not args:
-            print("** class name missing **")
-            return
-        if args[0] not in storage.classes():
-            print("** class doesn't exist **")
-            return
-        if len(args) < 2:
-            print("** instance id missing **")
-            return
-        key = "{}.{}".format(args[0], args[1])
-        if key in storage.all():
-            del storage.all()[key]
-            storage.save()
-        else:
-            print("** no instance found **")
-
-    def do_all(self, arg):
-        """Prints all string representation of all instances based or not on the class name"""
-        if arg and arg not in storage.classes():
-            print("** class doesn't exist **")
-            return
-        objects = [str(obj) for key, obj in storage.all().items() if not arg or arg == key.split('.')[0]]
-        print(objects)
-
-    def do_update(self, arg):
-        """Updates an instance based on the class name and id by adding or updating attribute"""
-        args = arg.split()
-        if not args:
-            print("** class name missing **")
-            return
-        if args[0] not in storage.classes():
-            print("** class doesn't exist **")
-            return
-        if len(args) < 2:
-            print("** instance id missing **")
-            return
-        key = "{}.{}".format(args[0], args[1])
-        if key not in storage.all():
-            print("** no instance found **")
-            return
-        if len(args) < 3:
-            print("** attribute name missing **")
-            return
-        if len(args) < 4:
-            print("** value missing **")
-            return
-        obj = storage.all()[key]
-        attr_name = args[2]
-        attr_value = args[3]
-        if attr_name in obj.__dict__:
-            attr_type = type(obj.__dict__[attr_name])
-            obj.__dict__[attr_name] = attr_type(attr_value)
-        else:
-            obj.__dict__[attr_name] = attr_value
-        obj.save()
+    # Add other commands (destroy, all, update) here
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
